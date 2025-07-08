43ed747d3f4f1907a6f7c45fd6d61cac827fbda2
Malik Zohaib Altaf <zohaibmalik1777@gmail.com>
2025-07-08 22:20:45 +0500

    images issue

--- src/components/RollDice.jsx
+++ src/components/RollDice.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './RollDice.css'
-import dice from '../../public/images/dice.png'
 
 const RollDice = ({currentDice, roledice}) => {
 
@@ -9,7 +8,7 @@ const RollDice = ({currentDice, roledice}) => {
   return (
     <div className='dicecontainer' onClick={roledice}>
       <div className='dice'>
-      <img src={`./images/dice_${currentDice}.png`} alt="dice 1" />
+      <img src={`./dice_${currentDice}.png`} alt="dice 1" />
       </div>
       <p>Clicl on Dice to Roll</p>
     </div>
